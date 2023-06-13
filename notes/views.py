4a82a9fff7ee9addc67d5c1282d21a2d29c3d7f0
Felipe Puentes <feliipepuentes@gmail.com>
2023-06-13 15:10:57 -0500

    adding auth and finishing course

--- notes/views.py
+++ notes/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render
-from django.http import Http404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http.response import HttpResponseRedirect
 
 from .models import Notes
+from .forms import NotesForm
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
+    login_url="/login"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name = "note"
+
+class NotesCreateView(CreateView):
+    model = Notes
+    form_class = NotesForm
+    success_url = '/smart/notes'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    form_class = NotesForm
+    success_url = '/smart/notes'
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
